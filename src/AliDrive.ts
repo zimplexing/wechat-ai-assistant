9bdeafd4694e6bc9a0eea7ea58af59306b01a6a5
Neil Zhang <neil.x.zhang@ringcentral.com>
2023-01-04 13:41:15 +0800

    update

--- src/AliDrive.ts
+++ src/AliDrive.ts
@@ -15,43 +15,43 @@ export default class AliDrive {
     await Utils.trySay(room ?? talker, `正在努力搜索 ${searchKey}...`)
     let message = ''
     let result = ''
-    const data = await fetch(`https://yiso.fun/api/search?name=${encodeURIComponent(searchKey)}&pageNo=1&pageSize=8`, {
-      headers: {
-        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,zh-HK;q=0.7,en;q=0.6,am;q=0.5,de-LI;q=0.4,de;q=0.3',
-        'cache-control': 'max-age=0',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1'
-      },
-      referrerPolicy: 'strict-origin-when-cross-origin',
-      body: null,
-      method: 'GET'
-    }).then(async res => await res.json()) as any
-    const list = data.data.list
-    // const data = await fetch('https://gitcafe.net/tool/alipaper/', {
+    // const data = await fetch(`https://yiso.fun/api/search?name=${encodeURIComponent(searchKey)}&pageNo=1&pageSize=8`, {
     //   headers: {
-    //     accept: '*/*',
+    //     accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     //     'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,zh-HK;q=0.7,en;q=0.6,am;q=0.5,de-LI;q=0.4,de;q=0.3',
-    //     'content-type': 'application/x-www-form-urlencoded',
+    //     'cache-control': 'max-age=0',
     //     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
     //     'sec-ch-ua-mobile': '?0',
     //     'sec-ch-ua-platform': '"macOS"',
-    //     'sec-fetch-dest': 'empty',
-    //     'sec-fetch-mode': 'cors',
-    //     'sec-fetch-site': 'same-site'
+    //     'sec-fetch-dest': 'document',
+    //     'sec-fetch-mode': 'navigate',
+    //     'sec-fetch-site': 'none',
+    //     'sec-fetch-user': '?1',
+    //     'upgrade-insecure-requests': '1'
     //   },
-    //   referrer: 'https://u.gitcafe.net/',
     //   referrerPolicy: 'strict-origin-when-cross-origin',
-    //   body: `action=search&keyword=${searchKey}`,
-    //   method: 'POST'
-    // }).then(async res => await res.json()) as Array<{ title: string, key: string }>
-    // const list = data.map(v => ({ name: v.title, url: `https://www.aliyundrive.com/s/${v.key}` }))
+    //   body: null,
+    //   method: 'GET'
+    // }).then(async res => await res.json()) as any
+    // const list = data.data.list
+    const data = await fetch('https://gitcafe.net/tool/alipaper/', {
+      headers: {
+        accept: '*/*',
+        'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,zh-HK;q=0.7,en;q=0.6,am;q=0.5,de-LI;q=0.4,de;q=0.3',
+        'content-type': 'application/x-www-form-urlencoded',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site'
+      },
+      referrer: 'https://u.gitcafe.net/',
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      body: `action=search&keyword=${searchKey}`,
+      method: 'POST'
+    }).then(async res => await res.json()) as Array<{ title: string, key: string }>
+    const list = data.map(v => ({ name: v.title, url: `https://www.aliyundrive.com/s/${v.key}` }))
     if (!list.length) {
       await Utils.trySay(room ?? talker, '抱歉，未找到资源')
       return
